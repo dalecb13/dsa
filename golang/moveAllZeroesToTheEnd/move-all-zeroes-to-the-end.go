da204862489d39a94d87a4aed2a97b9b8d207c2f
dalecb13 <dalecb13@gmail.com>
2025-08-13 11:33:59 -0400

    add other approaches for golang

--- golang/moveAllZeroesToTheEnd/move-all-zeroes-to-the-end.go
+++ golang/moveAllZeroesToTheEnd/move-all-zeroes-to-the-end.go
@@ -25,6 +25,33 @@ func moveZeroesNaive(nums []int) []int {
 	return temp
 }
 
+func holdCounter(nums []int) []int {
+	var leftPointer = 0
+	temp := make([]int, len(nums))
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			temp[leftPointer] = nums[i]
+			leftPointer++
+		}
+	}
+
+	return temp
+}
+
+func expectedTraversal(nums []int) []int {
+	var idx = 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[i], nums[idx] = nums[idx], nums[i]
+			idx++
+		}
+	}
+
+	return nums
+}
+
 // func moveZeroes(nums []int) []int {
 // 	var leftPointer = 0
 // 	var rightPointer = len(nums) - 1
@@ -36,4 +63,14 @@ func main() {
 	fmt.Println(moveZeroesNaive([]int{1, 2, 0, 4, 3, 0, 5, 0}))
 	fmt.Println(moveZeroesNaive([]int{10, 20, 30}))
 	fmt.Println(moveZeroesNaive([]int{0, 0}))
+
+	fmt.Println(holdCounter([]int{0, 1, 0, 3, 12}))
+	fmt.Println(holdCounter([]int{1, 2, 0, 4, 3, 0, 5, 0}))
+	fmt.Println(holdCounter([]int{10, 20, 30}))
+	fmt.Println(holdCounter([]int{0, 0}))
+
+	fmt.Println(expectedTraversal([]int{0, 1, 0, 3, 12}))
+	fmt.Println(expectedTraversal([]int{1, 2, 0, 4, 3, 0, 5, 0}))
+	fmt.Println(expectedTraversal([]int{10, 20, 30}))
+	fmt.Println(expectedTraversal([]int{0, 0}))
 }
